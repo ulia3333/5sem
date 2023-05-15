613bf8f7f4cf89316bb597e80a457457767ac054
Ulia <julia3 grin@gmail.com>
2023-05-16 00:53:04 +0300

    ЗИиНИС

---
+++ ЗИиНИС/LR/10/LempelZive/LempelZive/Triplet.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+// Структура кодов-троек для записи и чтения результатов сжатия
+struct Triplet
+{
+	int offset;
+	int length;
+	char next;
+
+	Triplet() = default;
+	Triplet(int off, int len, char next) : offset(off), length(len), next(next) {};
+	~Triplet() = default;
+};
+
