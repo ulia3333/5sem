de5ee6a0abb93cd1266ac10ad6572882e9cef5e7
Ulia <julia3 grin@gmail.com>
2023-05-16 01:50:31 +0300

    Тестирование

---
+++ Тестирование/9/ICanWin/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ICanWin
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
